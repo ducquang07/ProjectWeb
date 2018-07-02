1fde89d555389142dc778e6062f4eb782757fde9
ducquang07 <phanducquang07@gmail.com>
2018-07-02 22:38:24 +0700

    Fix lỗi ghép code

--- application/views/TrangChu_view.php
+++ application/views/TrangChu_view.php
@@ -753,7 +753,6 @@
 </script>
 
     <!-- Bootstrap core JavaScript
-<<<<<<< HEAD
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
@@ -772,23 +771,3 @@
     </script>
 </body>
 </html>
-=======
-    	================================================== -->
-    	<!-- Placed at the end of the document so the pages load faster -->
-    	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-    	<script src="<?php echo base_url() ?>vendor/jquery/jquery.min.js"></script>
-    	<script src="<?php echo base_url() ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    	<script type="text/javascript" src="<?php echo base_url() ?>vendor/lib/popper.min.js"></script>
-    	<!-- Plugin JavaScript -->
-    	<script src="<?php echo base_url() ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-    	<script src="<?php echo base_url() ?>vendor/scrollreveal/scrollreveal.min.js"></script>
-    	<script src="<?php echo base_url() ?>vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
-
-    	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    	<script type="text/javascript">
-    		var TabbedPanels1 = new Spry.Widget.TabbedPanels("TabbedPanels1");
-    	</script>
-    </body>
-    </html>
->>>>>>> 0479a3c5cdf10ca3aa0cc9c4c010bca2bd16c01a
